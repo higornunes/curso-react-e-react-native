40e335d5e301ab50404868ea90fa68670c4d0230
higornunes <higor.fernando.nunes@gmail.com>
2017-11-22 16:02:37 -0200

    master

--- app9/src/components/Conversas.js
+++ app9/src/components/Conversas.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+//teste git
 export default props => (
   <View style={{ flex: 1, backgroundColor: '#fc4349' }}  >
     <Text>Conversas</Text>
