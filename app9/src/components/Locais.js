619cfa6fa97a8dee061381ebf9466fdd5fa1328e
higornunes <higor.fernando.nunes@gmail.com>
2017-11-24 12:07:45 -0200

    arquivos deletados

--- app9/src/components/Locais.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-
-export default props => (
-  <View style={{ flex: 1, backgroundColor: '#fff' }} >
-    <Text>Locais</Text>
-  </View>
-);
